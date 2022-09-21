680ac231c0e51cebd946734b950f711f1194530c
iqbalalfa <ialfariandi@gmail.com>
2022-09-21 12:48:31 +0700

    tugas pertemuan 2

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 class contoh1 extends CI_Controller
 {
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Imam Nawawi
+        echo "Nama saya Imam Nawawi
         saya tinggal di daerah Ciputat
         olah raga yang saya sukai adalah Bulutangkis";
     }
